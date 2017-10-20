e720197bf493c4fd65c2fcc3b040d77060260c1b
yukun xie <jinyun2007@126.com>
2017-10-20 15:06:38 +0800

    增加底部导航栏及瀑布流

--- pages/hj_page/idx_hj.js
+++ pages/hj_page/idx_hj.js
@@ -1,66 +1,103 @@
-// pages/hj_page/idx_hj.js
+let col1H = 0;
+let col2H = 0;
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    data: {
+        scrollH: 0,
+        imgWidth: 0,
+        loadingCount: 0,
+        images: [],
+        col1: [],
+        col2: []
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    onLoad: function () {
+        wx.getSystemInfo({
+            success: (res) => {
+                let ww = res.windowWidth;
+                let wh = res.windowHeight;
+                let imgWidth = ww * 0.48;
+                let scrollH = wh;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+                this.setData({
+                    scrollH: scrollH,
+                    imgWidth: imgWidth
+                });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+                this.loadImages();
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    onImageLoad: function (e) {
+        let imageId = e.currentTarget.id;
+        let oImgW = e.detail.width;         //图片原始宽度
+        let oImgH = e.detail.height;        //图片原始高度
+        let imgWidth = this.data.imgWidth;  //图片设置的宽度
+        let scale = imgWidth / oImgW;        //比例计算
+        let imgHeight = oImgH * scale;      //自适应高度
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+        let images = this.data.images;
+        let imageObj = null;
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+        for (let i = 0; i < images.length; i++) {
+            let img = images[i];
+            if (img.id === imageId) {
+                imageObj = img;
+                break;
+            }
+        }
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+        imageObj.height = imgHeight;
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+        let loadingCount = this.data.loadingCount - 1;
+        let col1 = this.data.col1;
+        let col2 = this.data.col2;
+
+        if (col1H <= col2H) {
+            col1H += imgHeight;
+            col1.push(imageObj);
+        } else {
+            col2H += imgHeight;
+            col2.push(imageObj);
+        }
+
+        let data = {
+            loadingCount: loadingCount,
+            col1: col1,
+            col2: col2
+        };
+
+        if (!loadingCount) {
+            data.images = [];
+        }
+
+        this.setData(data);
+    },
+
+    loadImages: function () {
+        let images = [
+          { pic: "http://img.tuku.cn/file_thumb/201504/m2015041617184092.jpg", height: 0 },
+          { pic: "../../images/1.png", height: 0 },
+          { pic: "../../images/2.png", height: 0 },
+          { pic: "../../images/3.png", height: 0 },
+          { pic: "../../images/4.png", height: 0 },
+          { pic: "../../images/5.png", height: 0 },
+          { pic: "../../images/6.png", height: 0 },
+          { pic: "../../images/7.png", height: 0 }
+        ];
+
+        let baseId = "img-" + (+new Date());
+
+        for (let i = 0; i < images.length; i++) {
+            images[i].id = baseId + "-" + i;
+        }
+
+        this.setData({
+            loadingCount: images.length,
+            images: images
+        });
+    }
+
 })
